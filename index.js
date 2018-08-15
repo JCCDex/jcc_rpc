260e056d83a7bead873de52cfee32ac79407b90e
GinMu <gin.musheng@gmail.com>
2018-08-15 10:42:27 +0800

    update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var jcBiz = require('./lib/biz');
+var jcConfig = require('./lib/config');
+var jcExchange = require('./lib/exchange');
+var jcInfo = require('./lib/info');
+
+module.exports = {
+    jcBiz: jcBiz,
+    jcConfig: jcConfig,
+    jcExchange: jcExchange,
+    jcInfo: jcInfo
+}
