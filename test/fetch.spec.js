ccb206737a756ea33a544bccde80675a81a54141
GinMu <gin.musheng@gmail.com>
2019-12-03 12:59:46 +0800

    update test

--- test/fetch.spec.js
+++ test/fetch.spec.js
@@ -19,9 +19,11 @@ describe("test fetch", function() {
     });
     const spy = sandbox.spy(pendingTasks, "add")
     const spy1 = sandbox.spy(pendingTasks, "remove")
+    const spy2 = sandbox.spy(pendingTasks, "cancel")
     const res = await fetch.default({ url: "http://localhost/exchange/sign_order", method: "post" })
     expect(spy.called).to.true;
     expect(spy1.called).to.true;
+    expect(spy2.called).to.true;
     expect(res).to.deep.equal({
       result: true,
       code: '0',
@@ -73,5 +75,50 @@ describe("test fetch", function() {
       port: undefined,
       result: false
     })
+  })
+
+  it("test cancel", async () => {
+    const mock = new MockAdapter(fetch.default, { delayResponse: 100 });
+    mock.onPost('http://localhost/exchange/sign_order').reply(200, {
+      code: '0',
+      data: {
+        hash: "111"
+      }
+    }, {
+      date: "Tue Dec 03 2019 11:37:57 GMT+0800 (China Standard Time)"
+    });
+
+    mock.onPost('http://localhost/exchange/sign_order/test').reply(200, {
+      code: '0',
+      data: {
+        hash: "123"
+      }
+    }, {
+      date: "Tue Dec 03 2019 11:37:57 GMT+0800 (China Standard Time)"
+    });
+    let props = [fetch.default({ url: "http://localhost/exchange/sign_order", method: "post" }), fetch.default({ url: "http://localhost/exchange/sign_order", method: "post" })]
+    let res = await Promise.all(props);
+    expect(res).to.deep.nested.include({
+      host: undefined,
+      msg: 'pending request had been canceled',
+      port: undefined,
+      result: false
+    })
+    expect(pendingTasks.get()).to.deep.equal([]);
+    props = [fetch.default({ url: "http://localhost/exchange/sign_order", method: "post" }), fetch.default({ url: "http://localhost/exchange/sign_order/test", method: "post" })]
+    res = await Promise.all(props);
+    expect(res).to.deep.equal([{
+        result: true,
+        code: '0',
+        data: { hash: '111' },
+        date: 'Tue Dec 03 2019 11:37:57 GMT+0800 (China Standard Time)'
+      },
+      {
+        result: true,
+        code: '0',
+        data: { hash: '123' },
+        date: 'Tue Dec 03 2019 11:37:57 GMT+0800 (China Standard Time)'
+      }
+    ])
   })
 })
