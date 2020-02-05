7179b6020e13c9872921cf7732bcaa6669571ff4
GinMu <gin.musheng@gmail.com>
2020-02-05 14:19:21 +0800

    added node_rpc.ts

---
+++ src/node_rpc.ts
@@ -0,0 +1,136 @@
+import JcBase from "./base";
+const axios = require("axios");
+const service = axios.create({
+  timeout: 30000,
+  withCredentials: true
+});
+
+service.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+/**
+ * request data from node and submit data to node directly, instead of `JcExchange` class
+ *
+ * creation example:
+ *
+ * 1: `new JcNodeRpc(["http://localhost:8080", "https://localhost:8090"])`
+ *
+ * 2: `new JcNodeRpc(["localhost"], 8080, false)`
+ *
+ * @export
+ * @class JcNodeRpc
+ * @extends {JcBase}
+ */
+export default class JcNodeRpc extends JcBase {
+  constructor(...args) {
+    super(...args);
+  }
+
+  /**
+   * get sequence
+   *
+   * @param {string} address
+   * @returns {Promise<number>}
+   * @memberof JcNodeRpc
+   */
+  public async getSequence(address: string): Promise<number> {
+    const data = {
+      url: super.getUrl(),
+      data: {
+        method: "account_info",
+        params: [
+          {
+            account: address
+          }
+        ]
+      },
+      method: "post"
+    };
+    const res = await service(data);
+    const status = res?.result?.status;
+    if (status !== "success") {
+      throw new Error(res.result.error_message);
+    }
+
+    return res.result.account_data.Sequence;
+  }
+
+  /**
+   * create order with signed data
+   *
+   * @link [How to sign and create](https://github.com/JCCDex/jcc_exchange/blob/master/src/index.ts#L99)
+   * @param {string} blob
+   * @returns {Promise<unknown>}
+   * @memberof JcNodeRpc
+   */
+  public async createOrder(blob: string): Promise<unknown> {
+    return this.submit(blob);
+  }
+
+  /**
+   * cancel order with signed data
+   *
+   * @link [How to sign and cancel](https://github.com/JCCDex/jcc_exchange/blob/master/src/index.ts#L124)
+   * @param {string} blob
+   * @returns {Promise<unknown>}
+   * @memberof JcNodeRpc
+   */
+  public async cancelOrder(blob: string): Promise<unknown> {
+    return this.submit(blob);
+  }
+
+  /**
+   * transfer token with signed data
+   *
+   * @link [How to sign and transfer](https://github.com/JCCDex/jcc_exchange/blob/master/src/index.ts#L153)
+   * @param {string} blob
+   * @returns {Promise<unknown>}
+   * @memberof JcNodeRpc
+   */
+  public async transfer(blob: string): Promise<unknown> {
+    return this.submit(blob);
+  }
+
+  /**
+   * set brokerage with signed data
+   *
+   * @link [How to sign and set](https://github.com/JCCDex/jcc_exchange/blob/master/src/index.ts#L182)
+   * @param {string} blob
+   * @returns {Promise<unknown>}
+   * @memberof JcNodeRpc
+   */
+  public async setBrokerage(blob: string): Promise<unknown> {
+    return this.submit(blob);
+  }
+
+  /**
+   * submit transaction
+   *
+   * @protected
+   * @param {string} blob
+   * @returns {Promise<unknown>}
+   * @memberof JcNodeRpc
+   */
+  protected async submit(blob: string): Promise<unknown> {
+    const data = {
+      url: super.getUrl(),
+      data: {
+        method: "submit",
+        params: [
+          {
+            tx_blob: blob
+          }
+        ]
+      },
+      method: "post"
+    };
+    const res = await service(data);
+    return res;
+  }
+}
