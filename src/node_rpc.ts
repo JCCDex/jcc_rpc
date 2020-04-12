e2ca601ef55810c6eac2c476237257b1e60a39b0
chtian <chtian@anfengde.com>
2020-04-12 14:02:07 +0800

    add get signed transaction hash and sendRawTransaction

--- src/node_rpc.ts
+++ src/node_rpc.ts
@@ -133,6 +133,18 @@ export default class JcNodeRpc extends JcBase {
   }
 
   /**
+   * send raw transaction
+   *
+   * @link [How to sign and set](https://github.com/JCCDex/jcc_exchange/blob/master/src/index.ts#L182)
+   * @param {string} blob
+   * @returns {Promise<any>}
+   * @memberof JcNodeRpc
+   */
+  public async sendRawTransaction(blob: string): Promise<any> {
+    return this.submit(blob);
+  }
+
+  /**
    * submit transaction
    *
    * @protected
